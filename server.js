96486edd08bd9e80234d91cbc2cd8cea28f1a59b
Andres Patino <andrespatino0845@gmail.com>
2025-06-24 00:17:41 -0500

    Add dotenv dependency and update database configuration variables

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.use(express.json());
 
 const dbConfig = {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 };
+
 
 app.post('/api/clientes', async (req, res) => {
   try {
